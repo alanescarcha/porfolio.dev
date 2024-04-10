c608864db295aec7e61ccbe9631d5e4177d91fda
alanescarcha <alanescarcha11@gmail.com>
2024-04-10 05:25:22 -0300

    my version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://alanescarcha.com/',
+  output: "static",
+  adapter: vercel()
+});
